963dbe19ef5aa0bc67c78f68fadf19b82dd94e8b
Clutch06(Laptop) <kigun6651@gmail.com>
2018-10-11 11:45:55 +0900

    openGL cross

--- openGL_01/openGL_01/Source.cpp
+++ openGL_01/openGL_01/Source.cpp
@@ -1,7 +1,11 @@
 #include <gl/freeglut.h>
 
-GLvoid drawScene(GLvoid);
+GLvoid DrawScene(GLvoid);
 GLvoid Reshape(int w, int h);
+
+void Keyboard(unsigned char key, int x, int y);
+
+int status = 0;
 
 void main(int argc, char** argv)
 {
@@ -10,19 +14,36 @@ void main(int argc, char** argv)
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("openGL");
-	glutDisplayFunc(drawScene);
+	glutDisplayFunc(DrawScene);
 	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 }
 
-GLvoid drawScene()
+GLvoid DrawScene()
 {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glLineWidth(1.0);
+	glBegin(GL_LINES);
+		glVertex3f(375.0, 300.0, 0.0);
+		glVertex3f(425.0, 300.0, 0.0);
+		glVertex3f(400.0, 275.0, 0.0);
+		glVertex3f(400.0, 325.0, 0.0);
+	glEnd();
+
 	glFlush();
 }
 
 GLvoid Reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	glOrtho(0.0, 800.0, 0.0, 600.0, -400.0, 400.0);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	
 }
